a09bfe8b56ad30727e680a06f4b3b0d41613e26b
Oluwatodimu Adegoke <adegokeoluwatodimu@yahoo.com>
2024-07-30 11:23:52 -0500

    Added comments

--- board.py
+++ board.py
@@ -1,88 +1,110 @@
-# board class
+
 class Board:
-    def __init__(self,player1,player2):
-        self.board = [[player2,player2,player2],[0,0,0],[player1,player1,player1]]
+    def __init__(self, player1, player2):
+        """
+        Initializes a new instance of the Board class.
+        """
+        self.board = [[player2, player2, player2], [0, 0, 0], [player1, player1, player1]]
         self.player_pieces = 3
         self.ai_pieces = 3
         self.player1 = player1
         self.player2 = player2
-    # Returns the piece at a given row and column
+
     def get_piece_at(self, row, col):
+        """
+        Returns the piece at the given row and column.
+        Returns:
+        int: The piece at the given row and column.
+        """
         return self.board[row][col]
-    
-    # Moves the piece to the new row and column. Check if it's valid before moving
-    def move_piece(self,row, col, new_row, new_col,player):
-        if(player ==  self.player1 and self.get_piece_at(new_row, new_col) == self.player2):
+
+    def move_piece(self, row, col, new_row, new_col, player):
+        """
+        Moves the piece to the new row and column if the move is valid.
+        """
+        if player == self.player1 and self.get_piece_at(new_row, new_col) == self.player2:
             self.ai_pieces -= 1
-        elif(player == self.player2 and self.get_piece_at(new_row, new_col) == self.player1):    
+        elif player == self.player2 and self.get_piece_at(new_row, new_col) == self.player1:
             self.player_pieces -= 1
-            
+
         self.board[new_row][new_col] = self.board[row][col]
         self.board[row][col] = 0
 
-
-    # Checks if the move is valid by checking the current player, the position of the new colums and rows and also valid moves
-    def is_valid_move(self, row, col, new_row, new_col,player):
-        if(new_col < 0 or new_col > 2 or new_row < 0 or new_row > 2):
+    def is_valid_move(self, row, col, new_row, new_col, player):
+        """
+        Checks if the move is valid..
+        Returns:
+        bool: True if the move is valid, False otherwise.
+        """
+        if new_col < 0 or new_col > 2 or new_row < 0 or new_row > 2:
             return False
         if self.board[row][col] == self.board[new_row][new_col]:
             return False
-        if(self.board[row][col] != player):
+        if self.board[row][col] != player:
             return False
 
-        if(player == self.player1):
-            if(new_row == row - 1 and new_col == col and self.board[new_row][new_col] == 0):
+        if player == self.player1:
+            if new_row == row - 1 and new_col == col and self.board[new_row][new_col] == 0:
                 return True
-            if(new_row == row - 1 and new_col == col + 1 and self.board[new_row][new_col] == self.player2):
+            if new_row == row - 1 and new_col == col + 1 and self.board[new_row][new_col] == self.player2:
                 return True
-            if(new_row == row - 1 and new_col == col - 1 and self.board[new_row][new_col] == self.player2):
+            if new_row == row - 1 and new_col == col - 1 and self.board[new_row][new_col] == self.player2:
                 return True
             return False
         else:
-            if(new_row == row + 1 and new_col == col and self.board[new_row][new_col] == 0):
+            if new_row == row + 1 and new_col == col and self.board[new_row][new_col] == 0:
                 return True
-            if(new_row == row + 1 and new_col == col + 1 and self.board[new_row][new_col] == self.player1):
+            if new_row == row + 1 and new_col == col + 1 and self.board[new_row][new_col] == self.player1:
                 return True
-            if(new_row == row + 1 and new_col == col - 1 and self.board[new_row][new_col] == self.player1):
+            if new_row == row + 1 and new_col == col - 1 and self.board[new_row][new_col] == self.player1:
                 return True
             return False
-        
-    # Checks if the current player has won. Returns true if the current player has won, false otherwise
-    def check_win_condition(self,player):
-        if(player == self.player1):
-            if(self.board[0][0] == self.player1 or self.board[0][1] == self.player1 or self.board[0][2] == self.player1):
+
+    def check_win_condition(self, player):
+        """
+        Checks if the current player has won.
+        Returns:
+        bool: True if the current player has won, False otherwise.
+        """
+        if player == self.player1:
+            if self.board[0][0] == self.player1 or self.board[0][1] == self.player1 or self.board[0][2] == self.player1:
                 print("First row")
                 return True
-            if(self.ai_pieces == 0):
+            if self.ai_pieces == 0:
                 print("AI has no pieces")
                 return True
             return False
         else:
-            if(self.board[2][0] == self.player2 or self.board[2][1] == self.player2 or self.board[2][2] == self.player2 ):
+            if self.board[2][0] == self.player2 or self.board[2][1] == self.player2 or self.board[2][2] == self.player2:
                 print("Last row")
                 return True
-            if(self.player_pieces == 0):
+            if self.player_pieces == 0:
                 print("Player has no pieces")
                 return True
             return False
-        
-    def has_valid_moves(self,player):
+
+    def has_valid_moves(self, player):
+        """
+        Checks if the current player has any valid moves.
+        Returns:
+        bool: True if the current player has valid moves, False otherwise.
+        """
         for i in range(3):
             for j in range(3):
-                if(self.board[i][j] == player):
-                    if(player == self.player1):
-                        if(i - 1 >= 0 and self.board[i-1][j] == 0):
+                if self.board[i][j] == player:
+                    if player == self.player1:
+                        if i - 1 >= 0 and self.board[i - 1][j] == 0:
                             return True
-                        if(i - 1 >= 0 and j + 1 < 3 and self.board[i-1][j+1] == self.player2):
+                        if i - 1 >= 0 and j + 1 < 3 and self.board[i - 1][j + 1] == self.player2:
                             return True
-                        if(i - 1 >= 0 and j - 1 >= 0 and self.board[i-1][j-1] == self.player2):
+                        if i - 1 >= 0 and j - 1 >= 0 and self.board[i - 1][j - 1] == self.player2:
                             return True
                     else:
-                        if(i + 1 < 3 and self.board[i+1][j] == 0):
+                        if i + 1 < 3 and self.board[i + 1][j] == 0:
                             return True
-                        if(i + 1 < 3 and j + 1 < 3 and self.board[i+1][j+1] == self.player1):
+                        if i + 1 < 3 and j + 1 < 3 and self.board[i + 1][j + 1] == self.player1:
                             return True
-                        if(i + 1 < 3 and j - 1 >= 0 and self.board[i+1][j-1] == self.player1):
+                        if i + 1 < 3 and j - 1 >= 0 and self.board[i + 1][j - 1] == self.player1:
                             return True
         return False
 
--- game.py
+++ game.py
@@ -1,9 +1,17 @@
 import pygame
 from board import Board
 from player import Player
-from constants import PLAYER1_COLOR,PLAYER2_COLOR,LINE_COLOR,VALID_MOVE_COLOR
+from constants import PLAYER1_COLOR, PLAYER2_COLOR, LINE_COLOR, VALID_MOVE_COLOR
+
 class Game:
-    def __init__(self,screen):
+    """
+    Represents the game of HexaPawn.
+    """
+
+    def __init__(self, screen):
+        """
+        Initializes the Game object.
+        """
         self.screen = screen
         self.player1 = Player(1, "player1")
         self.player2 = Player(2, "ai")
@@ -14,8 +22,12 @@ class Game:
         self.last_move = None
         self.configurations = {}
 
-
-    def make_move(self,player, row, col):
+    def make_move(self, player, row, col):
+        """
+        Makes a move on the game board.
+        Returns:
+        - True if the move is valid and made successfully, False otherwise.
+        """
         if player.state == 0 and self.board.get_piece_at(row, col) == player:
             print("Picked up piece")
             player.state = 1
@@ -25,46 +37,69 @@ class Game:
             print("Dropped piece")
             player.state = 0
             return False
-        elif player.state == 1 and self.board.is_valid_move(player.picked_pos[0], player.picked_pos[1], row, col,self.current_player):
+        elif player.state == 1 and self.board.is_valid_move(player.picked_pos[0], player.picked_pos[1], row, col, self.current_player):
             print("Player 1 has moved")
-            self.board.move_piece(player.picked_pos[0], player.picked_pos[1], row, col,player)
+            self.board.move_piece(player.picked_pos[0], player.picked_pos[1], row, col, player)
             player.state = 0
             return True
         return False
 
     def board_to_tuple(self, board):
+        """
+        Converts the game board to a tuple.
+        Returns:
+        - A tuple representation of the game board.
+        """
         return tuple([tuple(row) for row in board])
-    
+
     def store_configuration(self, newBoard):
+        """
+        Stores a game configuration in the configurations dictionary.
+        """
         self.configurations[self.board_to_tuple(newBoard)] = True
 
     def check_configuration(self, newBoard):
+        """
+        Checks if a game configuration exists in the configurations dictionary.
+        Returns:
+        - True if the configuration exists, False otherwise.
+        """
         if self.board_to_tuple(newBoard) in self.configurations:
             return True
         return False
 
     def ai_move(self):
+        """
+        Makes a move for the AI player.
+        Returns:
+        - True if a move is made, False otherwise.
+        """
         for i in range(3):
             for j in range(3):
                 print("Checking piece at: " + str(i) + " " + str(j))
-                if self.board.get_piece_at(i,j) == self.current_player:
+                if self.board.get_piece_at(i, j) == self.current_player:
                     print("AI has picked up piece")
                     moves = [-1, 0, 1]
                     for k in moves:
                         if self.board.is_valid_move(i, j, i + 1, j + k, self.current_player):
                             newBoard = [row[:] for row in self.board.board]
                             newBoard[i][j] = 0
-                            newBoard[i+1][j+k] = self.current_player
+                            newBoard[i + 1][j + k] = self.current_player
                             if not self.check_configuration(newBoard):
                                 self.last_move = self.board_to_tuple(newBoard)
                                 pygame.time.wait(500)
-                                self.board.move_piece( i, j, i + 1, j + k,self.current_player)
+                                self.board.move_piece(i, j, i + 1, j + k, self.current_player)
                                 print("AI has moved")
                                 pygame.time.wait(500)
                                 return True
         return False
-    
-    def human_move(self,pos):
+
+    def human_move(self, pos):
+        """
+        Makes a move for the human player.
+        Returns:
+        - True if a move is made, False otherwise.
+        """
         if pos[0] > 150 or pos[1] > 150:
             return False
         hasMoved = False
@@ -74,7 +109,10 @@ class Game:
         hasMoved = self.make_move(self.current_player, row, col)
         return hasMoved
 
-    def finish_game(self,type):
+    def finish_game(self, type):
+        """
+        Finishes the game and updates the scores.
+        """
         if type == "player1":
             print("Player 1 has won")
             self.player1Score += 1
@@ -85,108 +123,132 @@ class Game:
         else:
             self.store_configuration(self.last_move)
             print("It's a draw")
-        print("AI: "+ str(self.player2Score) +" Player: "+ str(self.player1Score))
+        print("AI: " + str(self.player2Score) + " Player: " + str(self.player1Score))
         pygame.time.wait(1000)
         self.reset_game()
         return
-    
+
     def switch_turn(self):
+        """
+        Switches the turn between players.
+        """
         self.current_player = self.player2 if self.current_player == self.player1 else self.player1
-    
+
     def end_turn(self):
+        """
+        Ends the current turn and checks for game over conditions.
+        """
         if self.board.check_win_condition(self.current_player):
             winner = "player1" if self.current_player == self.player1 else "player2"
             self.finish_game(winner)
             return True
-        elif not  self.board.has_valid_moves(self.current_player):
+        elif not self.board.has_valid_moves(self.current_player):
             self.finish_game("draw")
             return True
         else:
             print("Switching turn")
             self.switch_turn()
         return False
-    
+
     def reset_game(self):
+        """
+        Resets the game to its initial state.
+        """
         self.board = Board(self.player1, self.player2)
         self.current_player = self.player1
 
     def handle_reset_button(self, pos):
+        """
+        Handles the reset button click.
+        Returns:
+        - True if the game is reset, False otherwise.
+        """
         if pos[0] > 170 and pos[0] < 200 and pos[1] > 115 and pos[1] < 145:
             self.reset_game()
             return True
         return False
 
     def draw(self):
+        """
+        Draws the game on the screen.
+        """
         self.draw_valid_move()
         self.draw_board()
         self.draw_pawns()
         self.draw_ui()
         self.draw_reset_button()
 
-     
-    # Draw the board
     def draw_board(self):
-        pygame.draw.line(self.screen, LINE_COLOR, (0,0), (150,0),2)
-        pygame.draw.line(self.screen, LINE_COLOR, (0,50), (150,50),2)
-        pygame.draw.line(self.screen, LINE_COLOR, (0,100), (150,100),2)
-        pygame.draw.line(self.screen, LINE_COLOR, (0,150), (150,150),2)
-        pygame.draw.line(self.screen, LINE_COLOR, (0,0), (0,150),2)
-        pygame.draw.line(self.screen, LINE_COLOR, (50,0), (50,150),2)
-        pygame.draw.line(self.screen, LINE_COLOR, (100,0), (100,150),2)
-        pygame.draw.line(self.screen, LINE_COLOR, (150,0), (150,150),2)
-    
-    # Draw the pawns on the board
+        """
+        Draws the game board on the screen.
+        """
+        pygame.draw.line(self.screen, LINE_COLOR, (0, 0), (150, 0), 2)
+        pygame.draw.line(self.screen, LINE_COLOR, (0, 50), (150, 50), 2)
+        pygame.draw.line(self.screen, LINE_COLOR, (0, 100), (150, 100), 2)
+        pygame.draw.line(self.screen, LINE_COLOR, (0, 150), (150, 150), 2)
+        pygame.draw.line(self.screen, LINE_COLOR, (0, 0), (0, 150), 2)
+        pygame.draw.line(self.screen, LINE_COLOR, (50, 0), (50, 150), 2)
+        pygame.draw.line(self.screen, LINE_COLOR, (100, 0), (100, 150), 2)
+        pygame.draw.line(self.screen, LINE_COLOR, (150, 0), (150, 150), 2)
+
     def draw_pawns(self):
+        """
+        Draws the pawns on the game board.
+        """
         for i in range(3):
             for j in range(3):
-                if(self.board.get_piece_at(i,j) == self.player1):
-                    pygame.draw.circle(self.screen, PLAYER1_COLOR, (j*50+25,i*50+25), 20)
-                elif(self.board.get_piece_at(i,j) == self.player2):
-                    pygame.draw.circle(self.screen, PLAYER2_COLOR, (j*50+25,i*50+25), 20)
-
+                if self.board.get_piece_at(i, j) == self.player1:
+                    pygame.draw.circle(self.screen, PLAYER1_COLOR, (j * 50 + 25, i * 50 + 25), 20)
+                elif self.board.get_piece_at(i, j) == self.player2:
+                    pygame.draw.circle(self.screen, PLAYER2_COLOR, (j * 50 + 25, i * 50 + 25), 20)
 
     def draw_reset_button(self):
+        """
+        Draws the reset button on the screen.
+        """
         font = pygame.font.Font("freesansbold.ttf", 12)
         button_rect = pygame.Rect(170, 115, 30, 30)
-        
+
         # Draw the button
         pygame.draw.rect(self.screen, (0, 0, 0), button_rect, 2)  # Border
-        
+
         # Draw the text
         text = font.render("R", True, (0, 0, 0))
         text_rect = text.get_rect(center=button_rect.center)
         self.screen.blit(text, text_rect)
 
-
     def draw_valid_move(self):
+        """
+        Draws the valid move overlay on the game board.
+        """
         if self.current_player.state == 1:
             row = self.current_player.picked_pos[0]
             col = self.current_player.picked_pos[1]
             overlay = pygame.Surface((48, 48), pygame.SRCALPHA)
-            
-            overlay.fill(VALID_MOVE_COLOR)  
+
+            overlay.fill(VALID_MOVE_COLOR)
             can_move = False
             if row - 1 >= 0 and self.board.board[row - 1][col] == 0:
-                self.screen.blit(overlay, (col*50+2, (row-1)*50 +2))
+                self.screen.blit(overlay, (col * 50 + 2, (row - 1) * 50 + 2))
                 can_move = True
             if row - 1 >= 0 and col + 1 < 3 and self.board.board[row - 1][col + 1] == self.player2:
-                self.screen.blit(overlay, ((col+1)*50+2, (row-1)*50+2))
+                self.screen.blit(overlay, ((col + 1) * 50 + 2, (row - 1) * 50 + 2))
                 can_move = True
             if row - 1 >= 0 and col - 1 >= 0 and self.board.board[row - 1][col - 1] == self.player2:
-                self.screen.blit(overlay, ((col-1)*50+2, (row-1)*50+2))
+                self.screen.blit(overlay, ((col - 1) * 50 + 2, (row - 1) * 50 + 2))
                 can_move = True
             if not can_move:
-                overlay.fill((255,0, 0, 100))
-                self.screen.blit(overlay, (col*50+2, (row)*50 +2))
-                
+                overlay.fill((255, 0, 0, 100))
+                self.screen.blit(overlay, (col * 50 + 2, (row) * 50 + 2))
 
-
-    # Draw the UI
     def draw_ui(self):
+        """
+        Draws the user interface on the screen.
+        """
         font = pygame.font.Font("freesansbold.ttf", 15)
-        textPlayer2 = font.render("p2 -  " + str(self.player2Score), True, (0,0,0))
-        textPlayer1 = font.render("p1 -  " + str(self.player1Score), True, (0,0,0))
-        textScore = font.render("SCORE", True, (0,0,0))
+        textPlayer2 = font.render("p2 -  " + str(self.player2Score), True, (0, 0, 0))
+        textPlayer1 = font.render("p1 -  " + str(self.player1Score), True, (0, 0, 0))
+        textScore = font.render("SCORE", True, (0, 0, 0))
         self.screen.blit(textScore, (160, 10))
         if self.current_player == self.player1:
             pygame.draw.circle(self.screen, PLAYER1_COLOR, (185, 90), 20)
@@ -194,4 +256,25 @@ class Game:
             pygame.draw.circle(self.screen, PLAYER2_COLOR, (185, 90), 20)
         self.screen.blit(textPlayer1, (165, 50))
         self.screen.blit(textPlayer2, (165, 30))
-        return 0
+        # Initialize the game screen
+        self.screen = screen
+
+        # Create player objects
+        self.player1 = Player(0)
+        self.player2 = Player(1)
+
+        # Set the current player to player 1
+        self.current_player = self.player1
+
+        # Create the game board
+        self.board = Board(self.player1, self.player2)
+
+        # Initialize player scores
+        self.player1Score = 0
+        self.player2Score = 0
+
+        # Initialize the last move
+        self.last_move = None
+
+        # Initialize the configurations dictionary
+        self.configurations = {}
